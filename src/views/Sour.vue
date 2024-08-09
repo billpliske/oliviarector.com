c265d1367d9fe0bb4127873ba7384d062e9bfc58
Bill Pliske <bpliske@gmail.com>
2024-08-08 18:33:47 -0700

    working videos page and working sour image guess page

--- src/views/Sour.vue
+++ src/views/Sour.vue
@@ -1,17 +1,144 @@
 <script setup>
+import { ref } from 'vue';
+
+// Step 1: Create a data set of 20 images
+const images = [
+  { src: 'image1.jpg', value: true },
+  { src: 'image2.jpg', value: true },
+  { src: 'image3.jpg', value: true },
+  { src: 'image4.jpg', value: true },
+  { src: 'image5.jpg', value: true },
+  { src: 'image6.jpg', value: true },
+  { src: 'image7.jpg', value: true },
+  { src: 'image8.jpg', value: true },
+  { src: 'image9.jpg', value: true },
+  { src: 'image10.jpg', value: true },
+  { src: 'image11.jpg', value: true },
+  { src: 'image12.jpg', value: true },
+  { src: 'image13.jpg', value: false },
+  { src: 'image14.jpg', value: false },
+  { src: 'image15.jpg', value: false },
+  { src: 'image16.jpg', value: false },
+  { src: 'image17.jpg', value: false },
+  { src: 'image18.jpg', value: false },
+  { src: 'image19.jpg', value: false },
+  { src: 'image20.jpg', value: false }
+];
+
+// Step 2: Randomly choose 10 images to display
+const selectedImages = ref([]);
+const resetImages = () => {
+  selectedImages.value = images.sort(() => 0.5 - Math.random()).slice(0, 10).map(image => ({
+    ...image,
+    selected: false
+  }));
+};
+resetImages();
+
+// Step 3: Toggle state for each image
+const toggleImage = (index) => {
+  selectedImages.value[index].selected = !selectedImages.value[index].selected;
+};
+
+// Step 4: Submit button to check user's selections
+const result = ref(null);
+
+
+const submit = () => {
+  const totalItems = selectedImages.value.length;
+  const totalTrueItems = selectedImages.value.filter(image => image.value).length;
+  const correctlyIdentified = selectedImages.value.filter(image => image.selected && image.value).length;
+  const incorrectChecks = selectedImages.value.filter(image => image.selected && !image.value).length;
+
+  // Calculate the percentage score based on correctly identified true items
+  const percentageScore = (correctlyIdentified / totalTrueItems) * 100 - (incorrectChecks / totalItems) * 100;
+
+  let grade;
+  if (percentageScore >= 90) {
+    grade = 'A';
+  } else if (percentageScore >= 80) {
+    grade = 'B';
+  } else if (percentageScore >= 70) {
+    grade = 'C';
+  } else if (percentageScore >= 60) {
+    grade = 'D';
+  } else {
+    grade = 'F';
+  }
+
+  result.value = `
+    Total Items: ${totalItems}
+    Total True Items: ${totalTrueItems}
+    Correctly Identified: ${correctlyIdentified}
+    Incorrect Checks: ${incorrectChecks}
+    Percentage Score: ${percentageScore.toFixed(2)}%
+    Grade: ${grade}
+  `;
+};
+
+
+// Step 5: Try again button to reset the game
+const tryAgain = () => {
+  result.value = null;
+  resetImages();
+};
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>sour</h1>
+    <h1>Sour</h1>
+    <div v-if="!result"
+         class="image-grid">
+      <div v-for="(image, index) in selectedImages"
+           :key="index"
+           class="image-item"
+           @click="toggleImage(index)">
+        <img :src="image.src"
+             :alt="'Image ' + (index + 1)" />
+        <div v-if="image.selected"
+             class="checkmark">✔️</div>
+      </div>
+    </div>
+    <button v-if="!result"
+            @click="submit">Submit</button>
+    <div v-if="result">
+      <pre>{{ result }}</pre>
+      <button @click="tryAgain">Try Again</button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  height: 100vh;
+  padding: 20px;
 }
-</style>e>
+
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: 10px;
+  width: 100%;
+}
+
+.image-item {
+  position: relative;
+  cursor: pointer;
+}
+
+.image-item img {
+  width: 100%;
+  height: auto;
+}
+
+.checkmark {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+  color: green;
+}
+</style>
--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,17 +1,67 @@
 <script setup>
+import { ref } from 'vue';
+
+const videoLinks = ref([
+  'https://www.youtube.com/embed/Udte0tu7IZw?si=-R3_cD5EwBoTn3tf',
+  'https://www.youtube.com/embed/WkLqgkW9h_w?si=4aRbd7ZeYTanXg7r',
+  'https://www.youtube.com/embed/wtzOKLD_b_A?si=o5f7RauPi6OJq6No',
+  'https://www.youtube.com/embed/DizGi6Q1MNE?si=gbWVYTd',
+  'https://www.youtube.com/embed/x6Fvg5ANYGM?si=qz7klh9n9PX5xB3w',
+  'https://www.youtube.com/embed/r63BsEuCyRE?si=D_lZtBe0j9wrEvt9',
+  'https://www.youtube.com/embed/sPmDv2obvjA?si=vplmdAySuIqmalH_',
+  'https://www.youtube.com/embed/QXcjPySjdJU?si=YiXLN2cqVu0doLy_',
+  'https://www.youtube.com/embed/xr_j3sT-bc8?si=GrGRCazGjkswU_4h',
+  'https://www.youtube.com/embed/t_ygEJu4SHs?si=1tThKq-_N2lM7CbD',
+  'https://www.youtube.com/embed/YB0qc0-Bzgs?si=gua8GiFu1WUJckHx',
+  'https://www.youtube.com/embed/bR-pos12iTA?si=SY3hqvfH7KoGFdKw',
+  'https://www.youtube.com/embed/r5AjFq4JQFQ?si=sqpyqM5T_qEttDvC',
+  'https://www.youtube.com/embed/GlM6lcFbLSg?si=DlEn38TpQf2Fhnr-',
+  'https://www.youtube.com/embed/OofRYPBwalI?si=FcEO1KJPGEH0Rfq9'
+]);
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>videos</h1>
+    <h1>Videos</h1>
+    <div class="video-grid">
+      <div v-for="(link, index) in videoLinks"
+           :key="index"
+           class="video-item">
+        <iframe :src="link"
+                frameborder="0"
+                allowfullscreen></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  height: 100vh;
+  padding: 20px;
+}
+
+.video-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  width: 100%;
+}
+
+.video-item {
+  position: relative;
+  padding-bottom: 56.25%;
+  /* 16:9 aspect ratio */
+  height: 0;
+}
+
+.video-item iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
